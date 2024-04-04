673183c02b99c61507023e0f0a4047ce596b1a51
Ajay Jagannatha Rao <anelamangala@vmware.com>
2024-04-04 13:47:57 +0530

    Fix and add application properties for all apps

--- scripts/deployToCloudFoundry.sh
+++ scripts/deployToCloudFoundry.sh
@@ -1,6 +1,6 @@
 echo "Creating Required Services..."
 {
-  cf create-service -c '{ "git": { "uri": "https://github.com/spring-petclinic/spring-petclinic-cloud-config.git", "periodic": true }, "count": 3 }' p.config-server standard config &
+  cf create-service -c '{ "git": { "uri": "https://github.com/njajay/spring-petclinic-cloud.git", "periodic": true }, "count": 3 }' p.config-server standard config &
   cf create-service p.service-registry standard registry & 
   cf create-service p.mysql db-small customers-db &
   cf create-service p.mysql db-small vets-db &
@@ -15,8 +15,8 @@ done
 mvn clean package -Pcloud
 cf push --no-start
 
-cf add-network-policy api-gateway --destination-app vets-service --protocol tcp --port 8080
-cf add-network-policy api-gateway --destination-app customers-service --protocol tcp --port 8080
-cf add-network-policy api-gateway --destination-app visits-service --protocol tcp --port 8080
+cf add-network-policy api-gateway vets-service --protocol tcp --port 8080
+cf add-network-policy api-gateway  customers-service --protocol tcp --port 8080
+cf add-network-policy api-gateway  visits-service --protocol tcp --port 8080
 
 cf start vets-service & cf start visits-service & cf start customers-service & cf start api-gateway &
